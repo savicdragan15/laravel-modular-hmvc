36706a4a1095388640606e4a24e1d95f210a0108
Dragan.Savic <Dragan.Savic@DRV16.mccanngroup.local>
2018-01-22 15:30:42 +0100

    -/ Change sub menu head

--- Modules/News/Resources/views/admin/partials/submenu.blade.php
+++ Modules/News/Resources/views/admin/partials/submenu.blade.php
@@ -25,7 +25,7 @@
                                         <span></span>
                                 </i>
                                 <span class="m-menu__link-text">
-                                       Crate Product
+                                       Crate Article
                                 </span>
                         </a>
                 </li>

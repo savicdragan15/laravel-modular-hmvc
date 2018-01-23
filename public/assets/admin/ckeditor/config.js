0c28ef7a6febf0b0898cf3baaba01a070334c019
Dragan.Savic <Dragan.Savic@DRV16.mccanngroup.local>
2018-01-23 10:28:50 +0100

    -/ plugins for ckeditor

--- public/assets/admin/ckeditor/config.js
+++ public/assets/admin/ckeditor/config.js
@@ -7,5 +7,9 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-	config.skin = 'office2013';
+	//config.skin = 'office2013';
+
+	config.extraPlugins = 'videoembed,embedbase,embed,notification,notificationaggregator';
+
+	config.embed_provider = '//ckeditor.iframe.ly/api/oembed?url={url}&callback={callback}';
 };

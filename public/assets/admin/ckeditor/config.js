ed06396391c7c70f21c6e27f70a829c569ba79ab
Dragan.Savic <Dragan.Savic@DRV16.mccanngroup.local>
2018-01-24 11:49:20 +0100

    -/ Add slide show image gallery CKeditor plugin

--- public/assets/admin/ckeditor/config.js
+++ public/assets/admin/ckeditor/config.js
@@ -9,7 +9,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// config.uiColor = '#AADC6E';
 	//config.skin = 'office2013';
 
-	config.extraPlugins = 'videoembed,embedbase,embed,notification,notificationaggregator,image2,lightbox';
+	config.extraPlugins = 'videoembed,embedbase,embed,notification,notificationaggregator,image2,lightbox,slideshow';
 	config.removeDialogTabs = 'link:upload;image2:Upload';
 	config.extraAllowedContent = 'a[data-lightbox,data-title,data-lightbox-saved]';
 	config.embed_provider = '//ckeditor.iframe.ly/api/oembed?url={url}&callback={callback}';
